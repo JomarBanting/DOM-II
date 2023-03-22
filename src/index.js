9b2c686df509ed64660a300ef8159589a91eb29d
Jomar Banting <shadowlover.05@gmail.com>
2023-03-22 01:02:32 -0400

    Added Eventlistener on ctrlKey

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './less/index.less'
 
 // Your code goes here!
+
+
+document.addEventListener("keydown", (event) => {
+    if (event.ctrlKey){
+          document.body.style.background = "#" + 
+          ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
+        }
+    })
+
+// let intro = document.querySelector(".intro");
+// intro.addEventListener("mouseover", (event) => {
+//     let standard = intro.style.fontWeight;
+//     if (event.type ==="mouseover") {
+//         intro.style.fontWeight = "bold";
+//     }
+//     if (event.type !== "mouseover"){
+//         intro.style.fontWeight = "thin";
+//     }
+// })
