6bb2734f42b42f357f0f73a6101f83593a606705
Jomar Banting <shadowlover.05@gmail.com>
2023-03-22 23:15:09 -0400

    Added the 'mouseover' and 'mouseout' event listener

--- src/index.js
+++ src/index.js
@@ -7,16 +7,34 @@ document.addEventListener("keydown", (event) => {
     if (event.ctrlKey){
           document.body.style.background = "#" + 
           ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
+          // random color generator that i got from stackoverflow.
+          // https://stackoverflow.com/questions/1484506/random-color-generator
         }
     })
 
-// let intro = document.querySelector(".intro");
-// intro.addEventListener("mouseover", (event) => {
-//     let standard = intro.style.fontWeight;
-//     if (event.type ==="mouseover") {
-//         intro.style.fontWeight = "bold";
-//     }
-//     if (event.type !== "mouseover"){
-//         intro.style.fontWeight = "thin";
-//     }
-// })
+function boldText(element) {
+    element.style.fontWeight = "bold";
+    element.style.color = "purple";
+}
+
+function unboldText(element) {
+    element.style.fontWeight = "normal";
+    element.style.color = "black";
+}
+
+let home = document.querySelector(".home");
+home.addEventListener("mouseover", (event) => {
+    boldText(event.target);
+});
+home.addEventListener("mouseout", (event) => {
+    unboldText(event.target);
+});
+
+let contentSelection = document.querySelector(".home");
+contentSelection.addEventListener("mouseover", (event) => {
+    boldText(event.target);
+});
+contentSelection.addEventListener("mouseout", (event) => {
+    unboldText(event.target);
+});
+
