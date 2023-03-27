9b3689a9ce34b6e87f3f1fcbb673362ede23df6c
Jomar Banting <shadowlover.05@gmail.com>
2023-03-27 02:42:07 -0400

    Trying to pass the npm run test

--- src/index.js
+++ src/index.js
@@ -11,6 +11,12 @@ document.addEventListener("keydown", (event) => {
           // https://stackoverflow.com/questions/1484506/random-color-generator
         }
     })
+let introTest = document.querySelector(".home .intro h2");
+console.log(introTest);
+    
+window.addEventListener("load", () => {
+    console.log("The page is fulled loaded"); 
+});
 
 function boldText(element) {
     element.style.fontWeight = "bold";
@@ -22,32 +28,48 @@ function unboldText(element) {
     element.style.color = "black";
 }
 
-let home = document.querySelector(".home");
-home.addEventListener("mouseover", (event) => {
-    boldText(event.target);
-});
-home.addEventListener("mouseout", (event) => {
-    unboldText(event.target);
-});
+const btnFocus = document.createElement("button");
+btnFocus.innerText = "Enter Your Name";
+let navContainer = document.querySelector(".nav-container");
+if (navContainer){
+    navContainer.appendChild(btnFocus);
+}
+let introH2Tag = document.querySelector(".intro h2");
+btnFocus.addEventListener("click", (event) => {
+    if (focusInputBox.value === ""){
+        event.preventDefault();
+        console.log("The Input Box is Empty!")
+    }
+    else {
+        introH2Tag.innerHTML = `Welcome To Fun Bus ${focusInputBox.value}!`;
+        introH2Tag.style.color = "red";
+    }
+})
 
 let contentSelection = document.querySelector(".home");
-contentSelection.addEventListener("mouseover", (event) => {
-    boldText(event.target);
-});
-contentSelection.addEventListener("mouseout", (event) => {
-    unboldText(event.target);
-});
+if (contentSelection){
+    contentSelection.addEventListener("mouseover", (event) => {
+        boldText(event.target);
+    });
+    contentSelection.addEventListener("mouseout", (event) => {
+        unboldText(event.target);
+    });
+}
+
 
 const wheelDetection = document.createElement("p");
 window.addEventListener("wheel", (event) => {
     const mouseWheel= event.textContent = "mouse wheel use deteted!";
-    document.querySelector("header").appendChild(wheelDetection);
+    introH2Tag.appendChild(wheelDetection);
     wheelDetection.textContent = mouseWheel;
 })
 
 let scrollCounter = 0;
 const scrollCounterText = document.createElement("p");
-document.querySelector("header").appendChild(scrollCounterText);
+if (scrollCounterText){
+    if (introH2Tag){
+        introH2Tag.appendChild(scrollCounterText);
+    }
 
 window.addEventListener("scroll", (event) => {
     
@@ -58,21 +80,19 @@ window.addEventListener("scroll", (event) => {
         scrollCounter+= 1;
     }
     scrollCounterText.textContent = `Scroll Counter: ${scrollCounter}`;
-    document.querySelector("header").appendChild(scrollCounterText);
+    introH2Tag.appendChild(scrollCounterText);
 })
-
-let introTest = document.querySelector(".home .intro h2");
-console.log(introTest);
-
-window.addEventListener("load", () => {
-   console.log("The page is fulled loaded"); 
-});
+}
 
 const focusInputBox = document.createElement("input");
 focusInputBox.setAttribute("id", "form");
 focusInputBox.type = "text";
 focusInputBox.innerText = "Enter your name"
-document.querySelector(".nav-container").appendChild(focusInputBox);
+if (navContainer)
+{
+    navContainer.appendChild(focusInputBox);
+}
+
 
 focusInputBox.addEventListener("focusin", (event) => {
     event.target.style.background = "pink";
@@ -82,20 +102,6 @@ focusInputBox.addEventListener("focusin", (event) => {
     event.target.style.background = "";
   });
 
-const btnFocus = document.createElement("button");
-btnFocus.innerText = "Enter Your Name";
-document.querySelector(".nav-container").appendChild(btnFocus);
 
-btnFocus.addEventListener("click", (event) => {
-    if (focusInputBox.value === ""){
-        event.preventDefault();
-        console.log("The Input Box is Empty!")
-    }
-    else {
-        let introH2Tag = document.querySelector(".intro h2");
-        introH2Tag.innerHTML = `Welcome To Fun Bus ${focusInputBox.value}!`;
-        introH2Tag.style.color = "red";
-    }
-})
 
 
